4261c9ef4ecd3c60448a4ded79c47a45bb952a52
sunil <sunil@hem.com>
2017-10-12 15:54:01 +0530

    CSRF Token Handling

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_stw_session'
+Rails.application.config.session_store :cookie_store
+Rails.application.config.action_dispatch.cookies_serializer = :marshal
