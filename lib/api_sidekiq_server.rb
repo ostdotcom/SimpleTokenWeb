9b36dc6ad398f9cf75c73b8e113b8d953703f9b7
bala007 <bala@pepo.com>
2017-10-12 15:55:59 +0530

    Sidekiq admin interface for ST API app - 2

--- lib/api_sidekiq_server.rb
+++ lib/api_sidekiq_server.rb
@@ -1,26 +1,6 @@
 require 'sidekiq/web'
 
 class ApiSidekiqServer < Sidekiq::Web
-  # before do
-  #   user = nil
-  #   # Get admin user from cookie
-  #   #_cookie_val = @env['rack.request.cookie_hash']['cookie_name']
-  #   user = true
-  #
-  #   # if user.blank?
-  #   #   redirect "/login?_frm=/admin/resque"
-  #   # elsif !user
-  #   #   halt(403)
-  #   # end
-  #
-  #   Sidekiq.configure_server do |config|
-  #     config.redis = {url: GlobalConstant::ApiSidekiq.redis_endpoint, namespace: GlobalConstant::ApiSidekiq.namespace}
-  #   end
-  #
-  #   Sidekiq.configure_client do |config|
-  #     config.redis = {url: GlobalConstant::ApiSidekiq.redis_endpoint, namespace: GlobalConstant::ApiSidekiq.namespace}
-  #   end
-  # end
 
   Sidekiq.configure_server do |config|
     config.redis = {url: GlobalConstant::ApiSidekiq.redis_endpoint, namespace: GlobalConstant::ApiSidekiq.namespace}
@@ -32,7 +12,7 @@ class ApiSidekiqServer < Sidekiq::Web
 
 end
 
-if Rails.env.production? || Rails.env.development?
+if Rails.env.production? Rails.env.staging? || Rails.env.development?
   Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
     user == GlobalConstant::ApiSidekiq.admin_user
     password == GlobalConstant::ApiSidekiq.admin_pw
