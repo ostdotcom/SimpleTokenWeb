ac94ae68113f21b1af11b08530689b998d1a8172
bala007 <bala@pepo.com>
2017-10-12 19:07:07 +0530

    host name validation for token sale

--- config/initializers/init_token_sale.rb
+++ config/initializers/init_token_sale.rb
@@ -2,9 +2,9 @@ class InitTokenSale
 
   def self.matches?(request)
     if Rails.env.production?
-      request.domain == 'sale.simpletoken.org'
+      request.host == 'sale.simpletoken.org'
     elsif Rails.env.staging?
-        request.domain == 'sale.stagingsimpletoken.org'
+      request.host == 'sale.stagingsimpletoken.org'
     else
       request.query_parameters[:initTokenSale].to_i == 1
     end
