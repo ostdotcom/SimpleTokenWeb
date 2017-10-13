fca74e1c0e73989512c3bd6f25c9bb3ddc1ea43f
thahir-pepo <thahir@pepo.com>
2017-10-13 21:45:07 +0530

    adding bootstrap select plugin

--- app/assets/javascripts/web/user-kyc-form.js
+++ app/assets/javascripts/web/user-kyc-form.js
@@ -1,4 +1,5 @@
 //= require plugins/jquery-file-upload/vendor/jquery.ui.widget
 //= require plugins/jquery-file-upload/jquery.fileupload
+//= require plugins/bootstrap_select/bootstrap-select
 //= require web/common/ModalHandling
 //= require web/user/KycForm
