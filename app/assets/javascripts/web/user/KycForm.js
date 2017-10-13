cbe37bda4d69835d8fb7703994a582f4a1e5b6e3
Akshay <akshay.raje@gmail.com>
2017-10-14 00:14:18 +0530

    KYC form upload

--- app/assets/javascripts/web/user/KycForm.js
+++ app/assets/javascripts/web/user/KycForm.js
@@ -8,23 +8,10 @@
   homeNs.kyc = oThis = {
 
       uploadParamsResponse: {},
+      uploadCount: 0,
 
       init: function (config) {
           oThis.bindButtonActions();
-
-          $('#fileupload').fileupload({
-              dataType: 'xml',
-              method: 'POST',
-              autoUpload: false,
-              singleFileUploads: false,
-              done: function (e, data) {
-                  $.each(oThis.uploadParamsResponse, function(upload_key, upload_value){
-                      if(upload_value.fields.key == data.formData.key){
-                          oThis.uploadParamsResponse[upload_key].status = data.jqXHR.status;
-                      }
-                  });
-              }
-          });
 
           //var filelist = [];
           //var paramNames = [];
@@ -53,6 +40,30 @@
 
       bindButtonActions: function () {
 
+          $('#fileupload').fileupload({
+              dataType: 'xml',
+              method: 'POST',
+              autoUpload: false,
+              singleFileUploads: false,
+              done: function (e, data) {
+                  $.each(oThis.uploadParamsResponse, function(upload_key, upload_value){
+                      if(upload_value.fields.key == data.formData.key){
+                          oThis.uploadParamsResponse[upload_key].status = data.jqXHR.status;
+                      }
+                  });
+                  oThis.uploadCount--;
+                  if(oThis.uploadCount == 0){
+                      oThis.submitData();
+                  }
+              },
+              progress: function(e, data){
+                  var percent = Math.round((data.loaded / data.total) * 100);
+                  $('#verifyModal .loader-content .progress .progress-bar')
+                      .css('width', percent + '%')
+                      .text(percent + '%');
+              },
+          });
+
           $("#kycSubmit").click(function (event) {
               event.preventDefault();
               oThis.validateForm();
@@ -66,7 +77,7 @@
                  $('#i_agree_terms_conditions').is(':checked') === true
              ) {
                  simpletoken.utils.errorHandling.clearFormErrors();
-                 oThis.submitForm();
+                 oThis.getSignedUrls();
              } else {
                  $('.error[data-for="verify_error"]').text('Please verify all above mentioned confirmations');
              }
@@ -105,7 +116,14 @@
           return fileParams;
       },
 
-      submitForm: function(){
+      getSignedUrls: function(){
+
+          $('#verifyModal .close').hide();
+          $('#verifyModal .verify-content').hide();
+          $('#verifyModal .loader-content .progress').hide();
+          $('#verifyModal .loader-content .status').text('Encrypting your upload data...');
+          $('#verifyModal .loader-content').show();
+
           $.ajax({
               url: '/api/user/upload-params/?'+oThis.makeFileParams(),
               success: function(response){
@@ -119,6 +137,10 @@
       },
 
       uploadFiles: function(data){
+
+          $('#verifyModal .loader-content .progress').show();
+          $('#verifyModal .loader-content .status').text('Uploading files via secured channel...');
+
           $.each(data, function(upload_key, upload_value){
               $('#fileupload').fileupload('send', {
                   files:[$('#kycForm input[name=passport_file_path]')[0].files[0]],
@@ -126,8 +148,46 @@
                   url: upload_value.url,
                   formData: upload_value.fields
               });
+              oThis.uploadCount++;
           });
-      }
+      },
+
+      submitData: function(){
+
+            $('#verifyModal .loader-content .progress').hide();
+            $('#verifyModal .loader-content .status').text('Submitting your KYC data...');
+
+            var $form = $('#kycForm');
+            var $data = $form.serializeArray().reduce(function(obj, item) {
+                  obj[item.name] = item.value;
+                  return obj;
+            }, {});
+            $.each(oThis.uploadParamsResponse, function(upload_key, upload_value){
+                $data[upload_key] = upload_value.fields.key;
+            });
+
+            $.ajax({
+                url: $form.attr('action'),
+                dataType: 'json',
+                method: $form.attr('method'),
+                data: $data,
+                success: function (response) {
+                    if (response.success == true) {
+                        window.location = '/reserve-token?initTokenSale=1';
+                        return false;
+                    } else {
+                        $('#verifyModal .close').show();
+                        $('#verifyModal .verify-content').show();
+                        $('#verifyModal .loader-content').hide();
+                        $('#verifyModal').modal('hide');
+                        simpletoken.utils.errorHandling.displayFormErrors(response);
+                    }
+                },
+                error: function (jqXHR, exception) {
+                    alert(utilsNs.errorHandling.xhrErrResponse(jqXHR, exception));
+                }
+            });
+      },
 
     //  submit: function () {
     //      var $form = $('#kycForm');
