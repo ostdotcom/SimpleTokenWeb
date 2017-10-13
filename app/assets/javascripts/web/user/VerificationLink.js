a18981d4d8b8e03f60786e11e147f436255bba56
aman_barbaria <aman@pepo.com>
2017-10-14 02:48:01 +0530

    resend double opt in integration

---
+++ app/assets/javascripts/web/user/VerificationLink.js
@@ -0,0 +1,47 @@
+;
+(function (window) {
+
+    var homeNs = ns("simpletoken.home"),
+        utilsNs = ns("simpletoken.utils"),
+        oThis;
+
+    homeNs.verification = oThis = {
+
+        init: function (config) {
+            oThis.bindButtonActions();
+        },
+
+        bindButtonActions: function () {
+
+            $("#resendLink").click(function (event) {
+                event.preventDefault();
+                oThis.resendEmail();
+            });
+
+        },
+
+        resendEmail: function () {
+            $.ajax({
+                url: 'api/user/resend-double-opt-in',
+                method: 'GET',
+                success: function (response) {
+                    if (response.success == true) {
+                        alert(response.message);
+                        return false;
+                    } else {
+                        alert(response.err.display_text);
+                    }
+                },
+                error: function (jqXHR, exception) {
+                    alert(utilsNs.errorHandling.xhrErrResponse(jqXHR, exception));
+                }
+            });
+        }
+
+    };
+
+    $(document).ready(function () {
+        oThis.init({i18n: {}});
+    });
+
+})(window);
