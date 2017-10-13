3376c57cee8adcf0d88a664130662b812964e13e
aman_barbaria <aman@pepo.com>
2017-10-14 01:21:03 +0530

    route configuration

--- config/initializers/init_token_sale.rb
+++ config/initializers/init_token_sale.rb
@@ -6,7 +6,7 @@ class InitTokenSale
     elsif Rails.env.staging?
       request.host == 'sale.stagingsimpletoken.org'
     else
-      request.host == 'sale.developmentstagingsimpletoken.org'
+      request.host == 'sale.developmentsimpletoken.org'
     end
   end
 
