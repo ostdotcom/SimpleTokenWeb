406cfa3706091a5cd38e2332c56d7195b1bba900
aman_barbaria <aman@pepo.com>
2017-10-15 16:17:13 +0530

    reset password success integration

--- app/assets/javascripts/web/user/ResetPassword.js
+++ app/assets/javascripts/web/user/ResetPassword.js
@@ -1,64 +1,80 @@
 ;
 (function (window) {
 
-  var homeNs = ns("simpletoken.home"),
-      utilsNs = ns("simpletoken.utils"),
-    oThis;
+    var homeNs = ns("simpletoken.home"),
+        utilsNs = ns("simpletoken.utils"),
+        oThis;
 
-  homeNs.reset = oThis = {
+    homeNs.reset = oThis = {
 
-      init: function (config) {
-          oThis.bindButtonActions();
-      },
+        init: function (config) {
+            oThis.bindButtonActions();
+        },
 
-      bindButtonActions: function () {
+        bindButtonActions: function () {
 
-          $("#recoverPassword").click(function (event) {
-              event.preventDefault();
-              var v = utilsNs.errorHandling.validationGeneric($('#userPasswordResetForm input[type="text"]'));
-              if (v === true) {
-                oThis.forgot_password();
-              }
-          });
+            $("#recoverPassword").click(function (event) {
+                event.preventDefault();
+                var v = utilsNs.errorHandling.validationGeneric($('#userPasswordResetForm input[type="text"]'));
+                if (v === true) {
+                    oThis.forgot_password(false);
+                    return false;
+                }
 
-          $("#resendLink").click(function (event) {
-              event.preventDefault();
-                  oThis.forgot_password();
-          });
+            });
 
-      },
+            $("#resendLink").click(function (event) {
+                event.preventDefault();
+                oThis.forgot_password(true);
+                return false;
+            });
 
-      forgot_password: function () {
-          var $form = $('#userPasswordResetForm');
-          $.ajax({
-              url: $form.attr('action'),
-              dataType: 'json',
-              method: $form.attr('method'),
-              data: $form.serialize(),
-              success: function (response) {
-                  if (response.success == true) {
-                      oThis.showSuccess();
-                      return false;
-                  } else {
-                    utilsNs.errorHandling.displayFormErrors(response);
-                  }
-              },
-              error: function (jqXHR, exception) {
-                utilsNs.errorHandling.xhrErrResponse(jqXHR, exception);
-              }
-          });
-      },
+        },
 
-      showSuccess: function () {
-         var userEmail = $('#email').val();
-          $('#emailSuccess').val(userEmail);
-          $('#resetPassword').hide();
-          $('#resetPasswordSuccess').show();
-      }
-  };
+        forgot_password: function (is_resend) {
+            $('#successMessage').hide().text('');
+            var $form = $('#userPasswordResetForm');
+            $.ajax({
+                url: $form.attr('action'),
+                dataType: 'json',
+                method: $form.attr('method'),
+                data: $form.serialize(),
+                success: function (response) {
+                    if (response.success == true) {
+                        if (is_resend == true){
+                            oThis.showSuccessMsg();
+                        }
+                        else {
+                            oThis.showSuccessPage();
+                        }
 
-  $(document).ready(function () {
-    oThis.init({i18n: {}});
-  });
+                        return false;
+                    } else {
+                        utilsNs.errorHandling.displayFormErrors(response);
+                    }
+                },
+                error: function (jqXHR, exception) {
+                    utilsNs.errorHandling.xhrErrResponse(jqXHR, exception);
+                }
+            });
+        },
+
+        showSuccessPage: function () {
+            var userEmail = $('#email').val();
+            $('#emailSuccess').val(userEmail);
+            $('#resetPassword').hide();
+            $('#resetPasswordSuccess').show();
+        },
+
+        showSuccessMsg: function () {
+            $('#successMessage').show().text('Reset Link has been sent!');
+        }
+
+
+    };
+
+    $(document).ready(function () {
+        oThis.init({i18n: {}});
+    });
 
 })(window);
