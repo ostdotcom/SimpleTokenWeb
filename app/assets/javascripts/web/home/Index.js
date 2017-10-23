2e1f2e5ea73716e06fe9a1ff2fb7335e9f067e99
aman_barbaria <aman@pepo.com>
2017-10-23 21:52:12 +0530

    pepo campaigns utm parameters attributes for email add in list

--- app/assets/javascripts/web/home/Index.js
+++ app/assets/javascripts/web/home/Index.js
@@ -1,114 +1,180 @@
 ;
 (function (window) {
 
-  var homeNs = ns("simpletoken.home"),
-    oThis;
+    var homeNs = ns("simpletoken.home"),
+        oThis;
 
-  homeNs.index = oThis = {
+    homeNs.index = oThis = {
 
-    init: function (config) {
-        oThis.bindButtonActions();
-    },
+        init: function (config) {
+            oThis.bindButtonActions();
+        },
 
-    bindButtonActions: function () {
+        bindButtonActions: function () {
 
-      $("#subscribe-form-submit").on("click", function(event){
-          event.preventDefault();
-          oThis.onSubscribe();
-      });
+            $("#subscribe-form-submit").on("click", function (event) {
+                event.preventDefault();
+                oThis.onSubscribe();
+            });
 
-      $(".smooth-scroll").on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800);
-        }
-      });
-
-      $("a[href='#subscribe']").on('click', function(event) {
-        $('#is_token_sale_user').prop('checked', true);
-      });
-
-    },
-
-
-  onSubscribe: function(){
-        var jsonpurl = $("#subscribe-form-submit").data('jsonp');
-        var email = $("#subscribe-form-email").val();
-
-        var attr_obj = {};
-        var errors = [];
-
-        if (email == '') {
-            errors.push('Email is Mandatory!');
-        }
-
-        if(errors.length > 0){
-            oThis.showError(errors.join(' '));
-            return false;
-        }
-
-        oThis.resetError();
-        $("#subscribe-form-submit").prop('disabled', true);
-
-        var attr_obj = {
-          is_general_update: 1,
-          is_token_sale_user: 1
-        };
-
-        $.ajax({
-
-            url: jsonpurl,
-            jsonp: "callback",
-            dataType: "jsonp",
-            data: {email: email, attributes: attr_obj},
-            method: 'GET',
-            success: function (responseJson) {
-                if ((responseJson.error != undefined) && (responseJson.error != '')) {
-
-                    var error_msg = [];
-                    $.each(responseJson.error_message, function (errors_key, errors_value) {
-                        $.each(errors_value, function (index, value) {
-                            error_msg.push(value);
-                        });
-                    });
-
-                    oThis.showError(error_msg.join('. '));
-
-                } else {
-
-                    oThis.resetError();
-                    $('#subscribe-form').hide();
-                    $('#subscribe-success').show();
+            $(".smooth-scroll").on('click', function (event) {
+                if (this.hash !== "") {
+                    event.preventDefault();
+                    var hash = this.hash;
+                    $('html, body').animate({
+                        scrollTop: $(hash).offset().top
+                    }, 800);
                 }
+            });
 
-            },
-            error: function (response) {
-                oThis.showError('Something Went Wrong');
-            },
-            complete: function(response) {
-                $("#subscribe-form-submit").prop('disabled', false);
+            $("a[href='#subscribe']").on('click', function (event) {
+                $('#is_token_sale_user').prop('checked', true);
+            });
+
+        },
+
+
+        onSubscribe: function () {
+            var jsonpurl = $("#subscribe-form-submit").data('jsonp');
+            var email = $("#subscribe-form-email").val();
+
+            var attr_obj = {};
+            var errors = [];
+
+            if (email == '') {
+                errors.push('Email is Mandatory!');
             }
 
-        });
-    },
+            if (errors.length > 0) {
+                oThis.showError(errors.join(' '));
+                return false;
+            }
 
-    showError: function(text){
-        $('#subscribe-form .error').html(text);
-        $('#subscribe-form-email').addClass('red');
-    },
+            oThis.resetError();
+            $("#subscribe-form-submit").prop('disabled', true);
 
-    resetError: function(){
-        $('#subscribe-form .error').html('');
-        $('#subscribe-form-email').removeClass('red');
-    }
+            var attr_obj = {
+                is_general_update: 1,
+                is_token_sale_user: 1
+            };
 
-  };
+            var utm_params = oThis.getUtmFromCookie();
+            Object.assign(attr_obj, utm_params);
 
-  $(document).ready(function () {
-    oThis.init({i18n: {}});
-  });
+            $.ajax({
+
+                url: jsonpurl,
+                jsonp: "callback",
+                dataType: "jsonp",
+                data: {email: email, attributes: attr_obj},
+                method: 'GET',
+                success: function (responseJson) {
+                    if ((responseJson.error != undefined) && (responseJson.error != '')) {
+
+                        var error_msg = [];
+                        $.each(responseJson.error_message, function (errors_key, errors_value) {
+                            $.each(errors_value, function (index, value) {
+                                error_msg.push(value);
+                            });
+                        });
+
+                        oThis.showError(error_msg.join('. '));
+
+                    } else {
+
+                        oThis.resetError();
+                        $('#subscribe-form').hide();
+                        $('#subscribe-success').show();
+                    }
+
+                },
+                error: function (response) {
+                    oThis.showError('Something Went Wrong');
+                },
+                complete: function (response) {
+                    $("#subscribe-form-submit").prop('disabled', false);
+                }
+
+            });
+        },
+
+        getUtmFromCookie: function () {
+            var name = "st_utm=";
+            var decodedCookie = decodeURIComponent(document.cookie);
+            var ca = decodedCookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return oThis.getUtmObjFromString(c.substring(name.length, c.length));
+                }
+            }
+            return {};
+        },
+
+
+        getUtmObjFromString: function (utm_string) {
+            var utm_keys = ['origin_page', 'utm_campaign', 'utm_content', 'utm_medium', 'utm_source', 'utm_term', 'utm_type']
+
+            var utm_parameters = {};
+            var str = '';
+            var key_str = '';
+            var val_str = '';
+
+            var quote_count = 0;
+
+            for (var i = 0; i < utm_string.length; i++) {
+                var c = utm_string.charAt(i);
+
+                if (c == '"') {
+                    if (quote_count > 0 && (utm_string.charAt(i - 1) == '\\')) {
+                        str = str + c;
+
+                    } else {
+                        quote_count = quote_count + 1;
+
+                        if (quote_count == 2) {
+                            key_str = str;
+                        }
+                        if (quote_count == 4) {
+                            val_str = str;
+                            if ((utm_keys.indexOf(key_str) > -1) && (val_str != '')) {
+                                utm_parameters[key_str] = val_str;
+                            }
+                            key_str = '';
+                            val_str = '';
+                            quote_count = 0;
+                        }
+
+                        str = '';
+                    }
+                }
+                else {
+                    if (quote_count != 0) {
+                        str = str + c;
+                    }
+                }
+            }
+            return utm_parameters;
+
+        },
+
+        showError: function (text) {
+            $('#subscribe-form .error').html(text);
+            $('#subscribe-form-email').addClass('red');
+        },
+
+        resetError: function () {
+            $('#subscribe-form .error').html('');
+            $('#subscribe-form-email').removeClass('red');
+        }
+
+    };
+
+    $(document).ready(function () {
+        oThis.init({i18n: {}});
+    });
 
 })(window);
--- app/assets/javascripts/web/marketing/Index.js
+++ app/assets/javascripts/web/marketing/Index.js
@@ -1,110 +1,177 @@
 ;
 (function (window) {
 
-  var marketingNs = ns("simpletoken.marketing"),
-    oThis;
+    var marketingNs = ns("simpletoken.marketing"),
+        oThis;
 
-  marketingNs.index = oThis = {
+    marketingNs.index = oThis = {
 
-    init: function (config) {
-      oThis.bindButtonActions();
-    },
+        init: function (config) {
+            oThis.bindButtonActions();
+        },
 
-    bindButtonActions: function () {
+        bindButtonActions: function () {
 
-      $("#subscribe-form-submit").on("click", function (event) {
-        event.preventDefault();
-        oThis.onSubscribe();
-      });
-
-      /* Assign empty url value to the iframe src attribute when
-       modal hide, which stop the video playing */
-      $("#youtube-video-modal").on('hide.bs.modal', function () {
-        $("#frameVideo").attr('src', '');
-      });
-
-      /* Assign the initially stored url back to the iframe src
-       attribute when modal is displayed again */
-      $("#youtube-video-modal").on('show.bs.modal', function (e) {
-        $("#frameVideo").attr('src', $(e.relatedTarget).data('src'));
-      });
-
-    },
-
-    onSubscribe: function () {
-      var jsonpurl = $("#subscribe-form-submit").data('jsonp');
-      var email = $("#subscribe-form-email").val();
-
-      var errors = [];
-
-      if (email == '') {
-        errors.push('Email is Mandatory!');
-      }
-
-      if (errors.length > 0) {
-        oThis.showError(errors.join(' '));
-        return false;
-      }
-
-      oThis.resetError();
-      $("#subscribe-form-submit").prop('disabled', true);
-
-      var attr_obj = {
-        is_general_update: 1,
-        is_token_sale_user: 1
-      };
-      $.ajax({
-
-        url: jsonpurl,
-        jsonp: "callback",
-        dataType: "jsonp",
-        data: {email: email, attributes: attr_obj},
-        method: 'GET',
-        success: function (responseJson) {
-          if ((responseJson.error != undefined) && (responseJson.error != '')) {
-
-            var error_msg = [];
-            $.each(responseJson.error_message, function (errors_key, errors_value) {
-              $.each(errors_value, function (index, value) {
-                error_msg.push(value);
-              });
+            $("#subscribe-form-submit").on("click", function (event) {
+                event.preventDefault();
+                oThis.onSubscribe();
             });
 
-            oThis.showError(error_msg.join('. '));
+            /* Assign empty url value to the iframe src attribute when
+             modal hide, which stop the video playing */
+            $("#youtube-video-modal").on('hide.bs.modal', function () {
+                $("#frameVideo").attr('src', '');
+            });
 
-          } else {
+            /* Assign the initially stored url back to the iframe src
+             attribute when modal is displayed again */
+            $("#youtube-video-modal").on('show.bs.modal', function (e) {
+                $("#frameVideo").attr('src', $(e.relatedTarget).data('src'));
+            });
+
+        },
+
+        onSubscribe: function () {
+            var jsonpurl = $("#subscribe-form-submit").data('jsonp');
+            var email = $("#subscribe-form-email").val();
+
+            var errors = [];
+
+            if (email == '') {
+                errors.push('Email is Mandatory!');
+            }
+
+            if (errors.length > 0) {
+                oThis.showError(errors.join(' '));
+                return false;
+            }
 
             oThis.resetError();
-            $('#subscribe-form').hide();
-            $('#subscribe-success').show();
-          }
+            $("#subscribe-form-submit").prop('disabled', true);
+
+            var attr_obj = {
+                is_general_update: 1,
+                is_token_sale_user: 1
+            };
+
+            var utm_params = oThis.getUtmFromCookie();
+            Object.assign(attr_obj, utm_params);
+
+            $.ajax({
+
+                url: jsonpurl,
+                jsonp: "callback",
+                dataType: "jsonp",
+                data: {email: email, attributes: attr_obj},
+                method: 'GET',
+                success: function (responseJson) {
+                    if ((responseJson.error != undefined) && (responseJson.error != '')) {
+
+                        var error_msg = [];
+                        $.each(responseJson.error_message, function (errors_key, errors_value) {
+                            $.each(errors_value, function (index, value) {
+                                error_msg.push(value);
+                            });
+                        });
+
+                        oThis.showError(error_msg.join('. '));
+
+                    } else {
+
+                        oThis.resetError();
+                        $('#subscribe-form').hide();
+                        $('#subscribe-success').show();
+                    }
+
+                },
+                error: function (response) {
+                    oThis.showError('Something Went Wrong');
+                },
+                complete: function (response) {
+                    $("#subscribe-form-submit").prop('disabled', false);
+                }
+
+            });
+        },
+
+
+        getUtmFromCookie: function () {
+            var name = "st_utm=";
+            var decodedCookie = decodeURIComponent(document.cookie);
+            var ca = decodedCookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return oThis.getUtmObjFromString(c.substring(name.length, c.length));
+                }
+            }
+            return {};
+        },
+
+
+        getUtmObjFromString: function (utm_string) {
+            var utm_keys = ['origin_page', 'utm_campaign', 'utm_content', 'utm_medium', 'utm_source', 'utm_term', 'utm_type']
+
+            var utm_parameters = {};
+            var str = '';
+            var key_str = '';
+            var val_str = '';
+
+            var quote_count = 0;
+
+            for (var i = 0; i < utm_string.length; i++) {
+                var c = utm_string.charAt(i);
+
+                if (c == '"') {
+                    if (quote_count > 0 && (utm_string.charAt(i - 1) == '\\')) {
+                        str = str + c;
+                    } else {
+                        quote_count = quote_count + 1;
+
+                        if (quote_count == 2) {
+                            key_str = str;
+                        }
+                        if (quote_count == 4) {
+                            val_str = str;
+                            if ((utm_keys.indexOf(key_str) > -1) && (val_str != '')) {
+                                utm_parameters[key_str] = val_str;
+                            }
+                            key_str = '';
+                            val_str = '';
+                            quote_count = 0;
+                        }
+
+                        str = '';
+                    }
+                }
+                else {
+                    if (quote_count != 0) {
+                        str = str + c;
+                    }
+                }
+            }
+            return utm_parameters;
 
         },
-        error: function (response) {
-          oThis.showError('Something Went Wrong');
+
+        showError: function (text) {
+            $('#subscribe-form .error').html(text);
+            $('#subscribe-form-email').addClass('border-error');
         },
-        complete: function (response) {
-          $("#subscribe-form-submit").prop('disabled', false);
+
+        resetError: function () {
+            $('#subscribe-form .error').html('');
+            $('#subscribe-form-email').removeClass('border-error');
         }
 
-      });
-    },
 
-    showError: function (text) {
-      $('#subscribe-form .error').html(text);
-      $('#subscribe-form-email').addClass('border-error');
-    },
+    };
 
-    resetError: function () {
-      $('#subscribe-form .error').html('');
-      $('#subscribe-form-email').removeClass('border-error');
-    }
-
-
-  };
-
-  $(document).ready(function () {
-    oThis.init({i18n: {}});
-  });
+    $(document).ready(function () {
+        oThis.init({i18n: {}});
+    });
 
 })(window);
