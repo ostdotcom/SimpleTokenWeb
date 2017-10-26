2efcd8e3bc2eab7be77d54c16e1eb3e1fd693f88
Akshay <akshay.raje@gmail.com>
2017-10-26 16:30:59 +0530

    Slick for courosal

--- app/assets/javascripts/web/home/Index.js
+++ app/assets/javascripts/web/home/Index.js
@@ -31,24 +31,6 @@
                 $('#is_token_sale_user').prop('checked', true);
             });
 
-
-            $('.carousel[data-type="multi"] .item').each(function(){
-                var next = $(this).next();
-                if (!next.length) {
-                    next = $(this).siblings(':first');
-                }
-                next.children(':first-child').clone().appendTo($(this));
-
-                for (var i=0;i<2;i++) {
-                    next=next.next();
-                    if (!next.length) {
-                        next = $(this).siblings(':first');
-                    }
-
-                    next.children(':first-child').clone().appendTo($(this));
-                }
-            });
-
         },
 
 
