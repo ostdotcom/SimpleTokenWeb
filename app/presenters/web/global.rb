0045545b53d6e7561ee0d4a380ca24fa8b8c16e6
aman_barbaria <aman@pepo.com>
2017-11-12 20:23:13 +0530

    bug fix

--- app/presenters/web/global.rb
+++ app/presenters/web/global.rb
@@ -30,6 +30,10 @@ module Presenters
         current_time >= GlobalConstant::StTokenSale.general_access_sale_start_date
       end
 
+      def has_early_access_sale_started?
+        current_time >= GlobalConstant::StTokenSale.early_access_sale_start_date
+      end
+
       def is_early_access_sale_on?
         (current_time >= GlobalConstant::StTokenSale.early_access_sale_start_date) && !has_general_access_sale_started?
       end
@@ -43,30 +47,11 @@ module Presenters
       end
 
       def has_sale_ended?
-        has_general_access_sale_started? && (current_time >= GlobalConstant::StTokenSale.general_access_sale_end_date || has_sale_ended_before_time?)
+        has_early_access_sale_started? && (current_time >= GlobalConstant::StTokenSale.general_access_sale_end_date || has_sale_ended_before_time?)
       end
 
       def has_sale_paused?
-        (token_sale_active_status.to_i != 1) && has_general_access_sale_started?
-      end
-
-      def progress_bar_percent
-        if has_sale_ended?
-          100
-        elsif has_general_access_sale_started?
-          66.66 + interval_percent(GlobalConstant::StTokenSale.general_access_sale_start_date, GlobalConstant::StTokenSale.general_access_sale_end_date)
-        elsif has_early_access_register_ended?
-          33.33 + interval_percent(GlobalConstant::StTokenSale.early_access_register_end_date, GlobalConstant::StTokenSale.general_access_sale_start_date)
-        else
-          interval_percent(GlobalConstant::StTokenSale.early_access_register_start_date, GlobalConstant::StTokenSale.early_access_register_end_date)
-        end
-      end
-
-      def interval_percent(start_date, end_date)
-        total_time_interval = end_date - start_date
-        time_passed = current_time - start_date
-        extra_percent = (time_passed*(33.33)/total_time_interval)
-        extra_percent
+        (token_sale_active_status.to_i != 1) && has_early_access_sale_started?
       end
 
       private
