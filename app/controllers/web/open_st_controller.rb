8e283f3a58344c38b86687150d2636506d47dad5
Akshay <akshay.raje@gmail.com>
2017-12-19 15:28:44 +0530

    Remove argument

--- app/controllers/web/open_st_controller.rb
+++ app/controllers/web/open_st_controller.rb
@@ -2,24 +2,9 @@ class Web::OpenStController < Web::BaseController
 
   layout "open_st"
 
-  before_action :allow_request, :set_page_meta_info
+  before_action :set_page_meta_info
 
   def index
-
-  end
-
-  private
-
-  def allow_request
-
-    unless params[:ar]=='allow_me'
-      if Rails.env.production?
-        redirect_to 'https://simpletoken.org'
-      else
-        redirect_to 'https://stagingsimpletoken.org'
-      end
-    end
-
 
   end
 
