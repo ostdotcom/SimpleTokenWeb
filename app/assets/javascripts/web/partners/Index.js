870ff59b88a1f28c6001d0cafb0580678561217c
thahir-pepo <thahir@pepo.com>
2018-01-18 20:58:14 +0530

    js for partners page

--- app/assets/javascripts/web/partners/Index.js
+++ app/assets/javascripts/web/partners/Index.js
@@ -1,33 +1,65 @@
 ;
 (function (window) {
 
-    var partnersNs = ns("simpletoken.partners"),
-        oThis;
+  var partnersNs = ns("simpletoken.partners"),
+      oThis;
 
-    partnersNs.index = oThis = {
+  partnersNs.index = oThis = {
 
-        init: function (config) {
-            oThis.bindButtonActions();
-        },
+      init: function (config) {
+          oThis.bindButtonActions();
+      },
 
-        bindButtonActions: function () {
+      bindButtonActions: function () {
 
-            $(".smooth-scroll").on('click', function (event) {
-                if (this.hash !== "") {
-                    event.preventDefault();
-                    var hash = this.hash;
-                    $('html, body').animate({
-                        scrollTop: $(hash).offset().top
-                    }, 800);
-                }
-            });
+        $("#partners-contact-us-submit").on("click", function (event) {
+          event.preventDefault();
+          oThis.onSendMessage();
+        });
 
-        }
+        $(".smooth-scroll").on('click', function (event) {
+          if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+          }
+        });
 
-    };
+      },
 
-    $(document).ready(function () {
-        oThis.init({i18n: {}});
-    });
+      onSendMessage: function () {
+        var contactusurl = $("#partners-contact-us").prop('action');
+
+        $("#partners-contact-us-submit").prop('disabled', true);
+
+        $.ajax({
+
+          url: contactusurl,
+          data: {},
+          method: 'POST',
+          success: function (responseJson) {
+            if () {
+
+
+            } else {
+
+            }
+
+          },
+          error: function (response) {
+          },
+          complete: function (response) {
+          }
+
+        });
+      }
+
+  };
+
+  $(document).ready(function () {
+      oThis.init({i18n: {}});
+  });
 
 })(window);
