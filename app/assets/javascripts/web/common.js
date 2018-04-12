8b373d022f1d0a52aff3c5f46f9414fd00f2c7b0
mayurpatil888 <mayur@ost.com>
2018-04-12 18:51:29 +0530

    Fix navbar on mobile view

--- app/assets/javascripts/web/common.js
+++ app/assets/javascripts/web/common.js
@@ -1,6 +1,6 @@
 //= require plugins/jquery/jquery
 //= require plugins/jquery-number/jquery.number
-//= require plugins/bootstrap/bootstrap
+//= require plugins/bootstrap-4/bootstrap
 //= require web/common/SimpleTokenNamespace
 //= require web/common/ErrorHandling
 //= require web/common/UtmHelper
