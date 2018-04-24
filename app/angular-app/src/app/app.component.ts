f8ee2c290d573a0be802351f372cb0b2b31332c2
Ashutosh <ashutosh@ost.com>
2018-04-24 21:33:59 +0530

    Setting csrf token

--- app/angular-app/src/app/app.component.ts
+++ app/angular-app/src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Http , BaseRequestOptions} from '@angular/http';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'Angular Data Table demo';
+  constructor( private http : Http  , private meta: Meta ) {
+    new RequestHeaders( http , meta );
+  }
 }
+
+class RequestHeaders extends BaseRequestOptions {
+  constructor( private http ,  private meta ){
+    super();
+    let csrf_token = meta.getTag('name=csrf-token').content;
+    http._defaultOptions.headers.set( 'X-CSRF-Token', csrf_token ); 
+  }
+}
+
+
+
+
+
+
+//Test code start
+function testCsrfAddition( http ){
+  http.get("api/admin/kyc/dashboard").subscribe( 
+    response => {
+      console.log("response"  , response); 
+    },  
+    error => {
+      console.log("error" , error); 
+    })
+}
+//Test code end
--- app/angular-app/src/app/filter/filter.component.ts
+++ app/angular-app/src/app/filter/filter.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css']
 })
+
+
 export class FilterComponent implements OnInit {
+
   @Input() filterInfo;
   @Input() dynamicVar;
   selectedValue = '';
 
-
   @Output() changeFilter = new EventEmitter<any>();
 
-  constructor() { }
+  constructor(http : Http) { 
+  }
 
   handleChange($event) {
     this.changeFilter.emit({id: this.filterInfo.id , selectedValue: this.selectedValue});
@@ -24,3 +28,5 @@ export class FilterComponent implements OnInit {
   }
 
 }
+
+
