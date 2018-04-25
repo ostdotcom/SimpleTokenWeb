6c2b00a72ecbe951d9686382eab7b0b41d326f48
thahir-pepo <thahir@pepo.com>
2018-04-25 16:29:47 +0530

    theme for admin

--- app/assets/javascripts/admin/common.js
+++ app/assets/javascripts/admin/common.js
@@ -1,5 +1,6 @@
 //= require plugins/jquery/jquery
-//= require plugins/bootstrap/bootstrap
+//= require plugins/bootstrap-4/bootstrap
+//= require plugins/popper/popper
 //= require web/common/SimpleTokenNamespace
 //= require admin/common/ErrorHandling
 //= require admin/common/CSRFAjax
