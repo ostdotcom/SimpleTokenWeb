1eb05d5a0a9faf212654d979fb6bd61f749b23ae
Akshay <akshay.raje@gmail.com>
2018-05-03 17:56:46 +0530

    KYC case component

--- app/angular-app/src/app/kyc-case/kyc-case.component.ts
+++ app/angular-app/src/app/kyc-case/kyc-case.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { OstHttp } from '../ost-http.service';
 
 @Component({
   selector: 'app-kyc-case',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KycCaseComponent implements OnInit {
 
-  constructor() { }
+  isProcessing: boolean = false;
+  caseDetails: object = {};
+  userDetails: object = {};
+  meta: object = {};
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: OstHttp
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.fetchCase(params.get('id'))
+    });
+  }
+
+  fetchCase(id) {
+    this.isProcessing = true;
+    this.http.get('api/admin/kyc/check-details', {
+      params: {
+        id: id
+      }
+    }).subscribe( response => {
+      this.onSuccess( response.json());
+    }, error => {
+
+    })
+  }
+
+  onSuccess( res ){
+    this.caseDetails = res.data.case_detail;
+    this.userDetails = res.data.user_detail;
+    this.meta = res.data.meta;
   }
 
 }
