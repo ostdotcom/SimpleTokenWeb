be7d91815c6c8f99e065cf80aaa7840993950a56
Akshay <akshay.raje@gmail.com>
2018-05-07 18:36:04 +0530

    update eth and report issue

--- app/angular-app/src/app/kyc-case/kyc-case.component.ts
+++ app/angular-app/src/app/kyc-case/kyc-case.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { OstHttp } from '../ost-http.service';
+import { RequestStateHandlerService } from '../request-state-handler.service';
 
 @Component({
   selector: 'app-kyc-case',
@@ -9,7 +10,11 @@ import { OstHttp } from '../ost-http.service';
 })
 export class KycCaseComponent implements OnInit {
 
-  isProcessing: boolean = false;
+  isProcessing: boolean = true;
+  hasError: boolean = false; 
+  showCase: boolean = true;
+  showReportIssue: boolean = false;
+  showUpdateEth: boolean = false;
   caseDetails: object = {};
   userDetails: object = {};
   meta: object = {};
@@ -17,7 +22,8 @@ export class KycCaseComponent implements OnInit {
   duplicateDataUrl: string = '';
   constructor(
     private route: ActivatedRoute,
-    private http: OstHttp
+    private http: OstHttp,
+    private stateHandler : RequestStateHandlerService
   ) { }
 
   ngOnInit() {
@@ -30,22 +36,29 @@ export class KycCaseComponent implements OnInit {
 
   fetchCase(id) {
     this.isProcessing = true;
-    this.http.get('api/admin/kyc/check-details', {
+    this.http.get('api/admin/kyc/check-details/', {
       params: {
         id: id
       }
     }).subscribe( response => {
       this.onSuccess( response.json());
     }, error => {
-
+      this.stateHandler.updateRequestStatus( this, false,  true , false ,  error.json()); 
     })
   }
 
   onSuccess( res ){
-    this.isProcessing = false;
     this.caseDetails = res.data.case_detail;
     this.userDetails = res.data.user_detail;
     this.meta = res.data.meta;
+    this.stateHandler.updateRequestStatus( this, false,  false);
+    this.showPageState();
+  }
+
+  showPageState(showCase = true, showReportIssue = false , showUpdateEth = false){
+        this.showCase = showCase;
+        this.showReportIssue = showReportIssue;
+        this.showUpdateEth = showUpdateEth;
   }
 
 }
