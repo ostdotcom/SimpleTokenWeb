f65b6c69549b7663d2ca5838028fc45238d9e8ec
mayurpatil888 <mayur@ost.com>
2018-05-07 16:49:30 +0530

    header refactoring

--- app/angular-app/src/app/kyc_user_header/header.component.ts
+++ app/angular-app/src/app/kyc_user_header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'dashboard-table-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss', '../ost-header/ost-header.component.scss']
 })
 export class HeaderComponent implements OnInit {
   @Input() header;
