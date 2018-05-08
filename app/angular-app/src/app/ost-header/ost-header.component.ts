52d23381dbcbb09682e728dc0aa24c1ca21ad94b
mayurpatil888 <mayur@ost.com>
2018-05-08 11:13:32 +0530

    remove unnecessary files

--- app/angular-app/src/app/ost-header/ost-header.component.ts
+++ app/angular-app/src/app/ost-header/ost-header.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ost-header',
   templateUrl: './ost-header.component.html',
   styleUrls: ['./ost-header.component.scss']
 })
-export class OstHeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class OstHeaderComponent{}
