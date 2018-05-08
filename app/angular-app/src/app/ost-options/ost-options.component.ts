f885fc94b5650aad185fdb99774c1ce41268064a
Ashutosh <ashutosh@ost.com>
2018-05-08 11:08:23 +0530

    select picker

--- app/angular-app/src/app/ost-options/ost-options.component.ts
+++ app/angular-app/src/app/ost-options/ost-options.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { EntityConfigService } from '../entity-config.service';
 
+declare var $:any;
+
 @Component({
   selector: '[ostOptions]',
   templateUrl: './ost-options.component.html',
@@ -16,6 +18,17 @@ export class OstOptionsComponent implements OnInit {
 
   ngOnInit() {
     this.options = this.entityConfigService.getEntityConfig(this.entityPath)['values'];
+  }
+
+  /*
+  * Selectpicker should always be initialized after the content for component has been render 
+  * ngAfterContentInit is a life cycle hook is trigger after the content init but dosent update the dom yet 
+  * So selectpicker is called once the dom is updated with all the options with setTimeOut 
+  */
+  ngAfterContentInit(){
+    setTimeout(function(){
+        $('.selectpicker').selectpicker();
+    } , 100); 
   }
 
 }
