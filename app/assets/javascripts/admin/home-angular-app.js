1baa962885ca3c58f75241e7ba6160fdbba73ff2
Ashutosh <ashutosh@ost.com>
2018-05-11 13:38:47 +0530

    staging build congiguration for staging

--- app/assets/javascripts/admin/home-angular-app.js
+++ app/assets/javascripts/admin/home-angular-app.js
@@ -1,2 +1,5 @@
-//= require web/angular-common.js
 //= require plugins/bootstrap_select/bootstrap-select.js
+//= require admin/angular/inline.bundle.js
+//= require admin/angular/polyfills.bundle.js
+//= require admin/angular/vendor.bundle.js
+//= require admin/angular/main.bundle.js
--- app/assets/javascripts/web/angular-common.js
+++
@@ -1,4 +0,0 @@
-//= require angular/inline.bundle.js
-//= require angular/polyfills.bundle.js
-//= require angular/vendor.bundle.js
-//= require angular/main.bundle.js
