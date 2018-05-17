92876266c7a14b55a5ebd70928eca7e12cfee905
mayurpatil888 <mayur@ost.com>
2018-05-17 12:42:13 +0530

    remove whitelist filter and fixes

--- app/angular-app/src/app/manage-user/manage-user.component.ts
+++ app/angular-app/src/app/manage-user/manage-user.component.ts
@@ -26,12 +26,11 @@ export class ManageUserComponent implements OnInit {
   defaultQueryParams: object = {
     'search[q]': '',
     'filters[kyc_submitted]': 'all',
-    'filters[whitelist_status]': 'all',
     'sortings[sort_by]': 'desc',
     'page_number': 1
   };
 
-  filterKeys: Array<any> = [ 'kyc_submitted', 'whitelist_status'];
+  filterKeys: Array<any> = [ 'kyc_submitted'];
   sortKeys: Array<any> = ['sort_by'];
   page_number: number;
   postApi: string;
@@ -39,6 +38,7 @@ export class ManageUserComponent implements OnInit {
   actionButtonClass: string;
   message: string;
   successMessage: string;
+  DataType: string;
   kyc_submitted: string;
   whitelist_status: string;
   sort_by: string;
@@ -57,37 +57,35 @@ export class ManageUserComponent implements OnInit {
 
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe((queryParams:any) => {
-      this.stateManage.init( this ); 
-      this.updateWhitelistFilter();
-    });
+      this.stateManage.init( this );
+     });
   }
 
   onFilterChange( filtersForm ) {
-    this.stateManage.onFilterChange( filtersForm ); 
-    this.updateWhitelistFilter();
-  }
+    this.stateManage.onFilterChange( filtersForm );
+   }
 
   onSortChange( sortForm ){
-    this.stateManage.onFilterChange( sortForm ); 
+    this.stateManage.onSortChange( sortForm );
   }
 
   onPageChange ( pageNumber ){
-    this.stateManage.onFilterChange( pageNumber ); 
+    this.stateManage.onPageChange( pageNumber );
   }
 
   onSearchSubmit(searchForm) {
-    this.stateManage.onSearch( searchForm ); 
+    this.stateManage.onSearch( searchForm );
   }
 
   onDeleteRow( user ){
     this.user = user;
     if (this.user.whitelist_status == 'done'){
-      this.postApi = 'case_reopen_api';
+      this.postApi = 'api/admin/kyc/open-case';
       this. actionBtnPrimaryName =  "RE-OPEN CASE";
       this.actionButtonClass = "case-reopen";
       this.message = "To delete a user who has already been qualified, you will need to reopen the case. Do you want to continue?";
       this.successMessage = "The case will be re-opened shortly. You will be able to delete the user once the case has been reopened."
-
+      this.DataType = 'case_id';
     }
     else{
       this.postApi = 'delete_api';
@@ -95,21 +93,14 @@ export class ManageUserComponent implements OnInit {
       this.actionButtonClass = "delete-user";
       this.message = "Attention! You are about to delete this user. This action is permanent and cannot be undone. Are you sure you want to continue?";
       this.successMessage = "User Deleted";
+      this.DataType = 'user_id';
 
     }
     $('#deleteUserModal').modal('show');
   }
 
   onDeleteRowSucces(e){
-    console.log(e);
     this.tableComponent.getTableData();
-  }
-
-  updateWhitelistFilter(){
-    this.isWhitelistDisabled = this.kyc_submitted == "no" ? true : false ;
-    setTimeout(function(){
-      $('.selectpicker').selectpicker('refresh');
-    }, 0);
   }
 
 
