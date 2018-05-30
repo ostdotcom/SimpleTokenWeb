464803c316a62b11568fc7e36c561336e0439ce1
amanbarbaria <aman@pepo.com>
2018-05-30 15:29:55 +0530

    'eusale.simpletoken.org' added in sale routes

--- config/initializers/init_token_sale.rb
+++ config/initializers/init_token_sale.rb
@@ -2,7 +2,7 @@ class InitTokenSale
 
   def self.matches?(request)
     if Rails.env.production?
-      request.host == 'sale.simpletoken.org'
+      ['sale.simpletoken.org', 'eusale.simpletoken.org'].include?(request.host)
     elsif Rails.env.sandbox?
       false #&& request.host == 'sale.sandboxost.com'
     elsif Rails.env.staging?
