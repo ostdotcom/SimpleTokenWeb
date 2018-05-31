e43aafe13cfa88cbfa4c1bfba6dd53bad95951b3
mayurpatil888 <mayur@ost.com>
2018-05-31 12:41:48 +0530

    entity config fix

--- app/angular-app/src/app/services/entity-config.service.ts
+++ app/angular-app/src/app/services/entity-config.service.ts
@@ -5,7 +5,7 @@ import { Response } from '@angular/http';
 @Injectable()
 export class EntityConfigService {
   entityConfig= {};
-  entityConfigUrl = 'entity_config.json/?ts='+ Date.now();
+  entityConfigUrl = 'entity_config.json?ts='+ Date.now();
   errorOccured: Boolean;
 
   constructor(private http: OstHttp) {}
