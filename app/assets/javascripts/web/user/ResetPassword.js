328aed60fd46410c6f0507842f3736f0b851b15c
Ashutosh <ashutosh@ost.com>
2018-06-01 11:28:04 +0530

    forget password and change password

--- app/assets/javascripts/web/user/ResetPassword.js
+++ app/assets/javascripts/web/user/ResetPassword.js
@@ -6,22 +6,27 @@
         oThis;
 
     homeNs.reset = oThis = {
+        jForm: null,
+        formHelper : null,
 
         init: function (config) {
-            oThis.bindButtonActions();
+          var oThis = this;
+          oThis.jForm = $('#userPasswordResetForm');
+          oThis.formHelper= oThis.jForm.formHelper({
+            success: function (response) {
+              if (response.success == true) {
+                if (is_resend == true){
+                  oThis.showSuccessMsg();
+                }
+                else {
+                  oThis.showSuccessPage();
+                }
+              }
+            }
+          });
         },
 
         bindButtonActions: function () {
-
-            $("#recoverPassword").click(function (event) {
-                event.preventDefault();
-                var v = utilsNs.errorHandling.validationGeneric($('#userPasswordResetForm input[type="text"]'));
-                if (v === true) {
-                    oThis.forgot_password(false);
-                    return false;
-                }
-
-            });
 
             $("#resendLink").click(function (event) {
                 event.preventDefault();
@@ -29,42 +34,6 @@
                 return false;
             });
 
-        },
-
-        forgot_password: function (is_resend) {
-            $("#recoverPassword")
-                .text('Recovering...')
-                .prop( "disabled", true );
-            $('#successMessage').hide().text('');
-            var $form = $('#userPasswordResetForm');
-            $.ajax({
-                url: $form.attr('action'),
-                dataType: 'json',
-                method: $form.attr('method'),
-                data: $form.serialize(),
-                success: function (response) {
-                    if (response.success == true) {
-                        if (is_resend == true){
-                            oThis.showSuccessMsg();
-                        }
-                        else {
-                            oThis.showSuccessPage();
-                        }
-
-                        return false;
-                    } else {
-                        utilsNs.errorHandling.displayFormErrors(response);
-                    }
-                },
-                error: function (jqXHR, exception) {
-                    utilsNs.errorHandling.xhrErrResponse(jqXHR, exception);
-                },
-                complete: function(){
-                    $("#recoverPassword")
-                        .text('Recover')
-                        .prop( "disabled", false );
-                }
-            });
         },
 
         showSuccessPage: function () {
