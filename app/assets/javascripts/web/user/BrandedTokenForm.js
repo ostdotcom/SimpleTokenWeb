6afac0baa887ccbd06e8093760dd90408c345f3a
mayurpatil888 <mayur@ost.com>
2018-06-04 11:33:57 +0530

    update token

--- app/assets/javascripts/web/user/BrandedTokenForm.js
+++ app/assets/javascripts/web/user/BrandedTokenForm.js
@@ -11,46 +11,16 @@
 
       init: function (config) {
           oThis.redirectLocation = config.redirect_location;
-          oThis.bindButtonActions();
-      },
 
-      bindButtonActions: function () {
 
-          $("#btSubmit").click(function (event) {
-              event.preventDefault();
-              var v = utilsNs.errorHandling.validationGeneric($('#userBtForm input[type="text"]'));
-              if (v === true) {
-                oThis.submit(false);
-              }
-          });
 
-          $("#btSkip").click(function (event) {
-              event.preventDefault();
-              oThis.submit(true);
-          });
-
-      },
-
-      submit: function (skip_name) {
-          var $form = $('#userBtForm');
-          data = $form.serialize();
-          data += "&skip_name=" + skip_name;
-
-          $.ajax({
-              url: $form.attr('action'),
-              dataType: 'json',
-              method: $form.attr('method'),
-              data: data,
-              success: function (response) {
+          oThis.brandedTokenForm = $('#userBtForm');
+          oThis.formHelper = oThis.brandedTokenForm.formHelper({
+              success : function ( response ) {
                   if (response.success == true) {
                       window.location = oThis.redirectLocation;
                       return false;
-                  } else {
-                    utilsNs.errorHandling.displayFormErrors(response);
                   }
-              },
-              error: function (jqXHR, exception) {
-                utilsNs.errorHandling.xhrErrResponse(jqXHR, exception);
               }
           });
       }
