5794a985da93a983ede89e2d2a301b5bd2c5d3e8
mayurpatil888 <mayur@ost.com>
2018-07-06 11:33:48 +0530

    form error handler

---
+++ app/angular-app/src/app/ost-form-error-handler/ost-form-error-handler.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ost-form-error-handler',
+  templateUrl: './ost-form-error-handler.component.html',
+  styleUrls: ['./ost-form-error-handler.component.scss']
+})
+export class OstFormErrorHandlerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
