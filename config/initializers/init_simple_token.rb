6a0981c641a2780eb374d7daf7fa5dbafe954d49
Pankaj Keswani <pankaj@Pankajs-MacBook-Pro-2.local>
2018-07-23 15:14:17 +0530

    Simple token redirect issue

--- config/initializers/init_simple_token.rb
+++ config/initializers/init_simple_token.rb
@@ -2,13 +2,13 @@ class InitSimpleToken
 
   def self.matches?(request)
     if Rails.env.production?
-      request.host == 'simpletoken.org'
+      request.host == 'simpletoken.org' || request.host == 'www.simpletoken.org'
     elsif Rails.env.sandbox?
       false #&& request.host == 'sale.sandboxost.com'
     elsif Rails.env.staging?
-      request.host == 'stagingsimpletoken.org'
+      request.host == 'stagingsimpletoken.org' || request.host == 'www.stagingsimpletoken.org'
     else
-      request.host == 'developmentsimpletoken.org'
+      request.host == 'developmentsimpletoken.org' || request.host == 'www.developmentsimpletoken.org'
     end
   end
 
