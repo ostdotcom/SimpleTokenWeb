0b1cdbdfb20179164a97862883643d91acdf9516
Ashutosh <ashutosh@ost.com>
2018-08-08 12:22:21 +0530

    ost-file-upload done

--- app/assets/javascripts/plugins/helpers/handlebarHelper.js
+++ app/assets/javascripts/plugins/helpers/handlebarHelper.js
@@ -15,6 +15,36 @@
       return jMarkup ;
     }
 
-  }
+  };
+
+
+
+  Handlebars.registerHelper('is_required', function(data, options ) {
+    if( data == 1 ){
+      return "required";
+    }else {
+      return "";
+    }
+  });
+
+  Handlebars.registerHelper('ifTooltip', function(tooltip, options ) {
+    if( !!tooltip ){
+      return options.fn(this);
+    }
+
+    return options.inverse(this);
+  });
+
+  var idCount = 1 ;
+  Handlebars.registerHelper('get_input_id', function( name, isSameId , options ) {
+    if( !isSameId ) {
+      idCount++
+    }
+    if( name ){
+      return name + "_" + idCount ;
+    }else {
+      return "no_name" + "_" + idCount ;
+    }
+  });
 
 })(window );
