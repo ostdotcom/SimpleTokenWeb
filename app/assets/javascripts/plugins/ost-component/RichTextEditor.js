6d742fa325c283ee73d9ea5bb63d2327fb67b15a
Ashutosh <ashutosh@ost.com>
2018-08-27 16:15:14 +0530

    copy paste rich text editor

--- app/assets/javascripts/plugins/ost-component/RichTextEditor.js
+++ app/assets/javascripts/plugins/ost-component/RichTextEditor.js
@@ -7,7 +7,7 @@
 
   var tinyMceBodyClass = "tiny-mce-editor-inline-body",
       tinyMceBodyStyle = ".tiny-mce-editor-inline-body { color: #597A84; font-size: 0.75rem; font-weight: 300; letter-spacing: 0.2px; font-family: 'Quasimoda', Source Sans Pro, sans-serif;}",
-      pluginOptions    = ['link'] ,
+      pluginOptions    = ['link', 'paste'] ,
       toolbarOptions   = 'bold, italic, underline, link'
   ;
 
@@ -23,14 +23,15 @@
       toolbar_items_size      : "small",
       extended_valid_elements : "*[*]",
       entity_encoding         : 'raw',
+      paste_as_text           : true,
 
       setup: function (editor) {
           editor.on('keyup change', function () {
-            var jTextArea       = getTextArea( tinymce.activeEditor ),
-                updatedContent  = tinymce.activeEditor.getContent()
+            var jTextArea = getTextArea( tinymce.activeEditor ),
+                content   = tinymce.activeEditor.getContent()
             ;
             jTextArea && jTextArea
-              .val( updatedContent )
+              .val( content )
               .trigger('keyup')
               .trigger('change')
             ;
