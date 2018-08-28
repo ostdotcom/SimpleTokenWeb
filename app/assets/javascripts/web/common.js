c2d3af0e86cf7d4ca63e3f94a7facc001dc4e0b3
thahir-pepo <thahir@pepo.com>
2018-08-28 13:44:02 +0530

    adding responsive lazy load plugin

--- app/assets/javascripts/web/common.js
+++ app/assets/javascripts/web/common.js
@@ -1,6 +1,7 @@
 //= require plugins/jquery/jquery
 //= require plugins/jquery-number/jquery.number
 //= require plugins/bootstrap/bootstrap
+//= require plugins/responsively_lazy/responsivelyLazy
 //= require web/common/SimpleTokenNamespace
 //= require web/common/ErrorHandling
 //= require web/common/UtmHelper
