ee21cbc2d734b8776801212d637ed6c0c77f9bd0
Preshita Shirke <preshita@ost.com>
2018-10-23 12:11:38 +0530

    on click of settings, redirect to time settings when contract addresses page is not accessible

--- app/angular-app/src/app/navbar/navbar.component.ts
+++ app/angular-app/src/app/navbar/navbar.component.ts
@@ -8,11 +8,25 @@ import { AppConfigService } from '../services/app-config.service';
 })
 export class NavbarComponent implements OnInit {
 
+  web_host_setup_done  :boolean  = false;
+  has_whitelist_add_on :boolean  = false;
+  isSuperAdmin         :boolean  = false;
+
   constructor(
     public appConfig : AppConfigService
   ) { }
 
   ngOnInit() {
+    this.web_host_setup_done  = this.appConfig.hasClientOpted();
+    this.has_whitelist_add_on = this.appConfig.hasWhitelistAddOn();
+    this.isSuperAdmin         = this.appConfig.isSuperAdmin();
+  }
+
+  getSettingsRoute() {
+    if( this.isSuperAdmin && (this.web_host_setup_done || this.has_whitelist_add_on) ) {
+      return '/admin/settings/contract-addresses';
+    }
+    return '/admin/settings/sale-settings';
   }
 
 }
