3c58a7619786cb97dfb1a0636f4a4a97f11ebacd
Preshita Shirke <preshita@ost.com>
2019-01-15 20:57:52 +0530

    Minor fix

--- app/angular-app/src/app/manage-user/update-eth-modal/update-eth-modal.component.ts
+++ app/angular-app/src/app/manage-user/update-eth-modal/update-eth-modal.component.ts
@@ -12,33 +12,31 @@ export class UpdateEthModalComponent implements OnInit {
 
   @Input('user') user;
 
-  data  : Object =  {};
-  errorResponse: Object = {}
+  errorResponse: Object = {};
   postUrl : string = 'api/admin/kyc/update-address';
   updateBtnText : string = 'Update';
   isUpdating : boolean = false;
-  isSuccess  : boolean = false ; 
+  isSuccess  : boolean = false ;
 
   constructor(private http: OstHttp) { }
 
   ngOnInit() {
     $("#updateEthAddressModal").off('hidden.bs.modal').on("hidden.bs.modal", () => {
-      this.isUpdating = false ; 
-      this.isSuccess = false ; 
+      this.isUpdating = false ;
+      this.isSuccess = false ;
       this.updateBtnText = 'Update';
-      this.errorResponse = {}; 
+      this.errorResponse = {};
     });
   }
 
   updateEthAddress( updateEthAdressForm ) {
-    let ethAddress = updateEthAdressForm.value.address.trim(), 
+    let data = updateEthAdressForm.value,
         caseId = this.user && this.user['case_id']
         ;
-    this.data['id'] = caseId;
-    this.data['ethereum_address'] = ethAddress; 
+    data['id'] = caseId;
     this.isUpdating = true;
     this.updateBtnText = 'Updating ...';
-    this.http.post(this.postUrl, this.data).subscribe(
+    this.http.post(this.postUrl, data).subscribe(
       response => {
         let res = response.json();
         if (res.success) {
@@ -55,17 +53,22 @@ export class UpdateEthModalComponent implements OnInit {
 
   onSuccess( res ) {
     this.onComplete();
-    this.isSuccess = true ; 
+    this.isSuccess = true ;
   }
 
   onError( res ){
-    this.errorResponse = res ; 
+    this.errorResponse = res ;
     this.onComplete();
   }
 
   onComplete(){
     this.isUpdating = false;
     this.updateBtnText = 'update';
+  }
+
+  resetForm( form ){
+    form.reset();
+    $("#updateEthAddressModal").modal('hide');
   }
 
 }
