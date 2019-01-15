1afeb88e1d89d5ab4dd3daf94b803a6495d00a17
Preshita Shirke <preshita@ost.com>
2019-01-15 21:23:24 +0530

    minor change

--- app/angular-app/src/app/manage-user/manage-user-row/manage-user-row.component.ts
+++ app/angular-app/src/app/manage-user/manage-user-row/manage-user-row.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AppConfigService } from '../../services/app-config.service';
+import {EntityConfigService} from "../../services/entity-config.service";
 
 
 declare var $: any;
@@ -13,13 +14,14 @@ export class ManageUserRowComponent implements OnInit {
 
   @Output("deleteRowEvent") deleteRowEvent = new EventEmitter();
   @Output("updatedEthAddressEvent") updatedEthAddressEvent = new EventEmitter();
-  
+
   @Input() row;
-  constructor(public appConfigService: AppConfigService) { }
+  constructor(public appConfigService: AppConfigService,
+              private entityConfigService : EntityConfigService) { }
 
   entityPath: string;
   status: any;
-  jUpdateEthAddressModal :any = $('#updateEthAddressModal');
+  options: any;
 
   ngOnInit() {
     setTimeout(function(){
@@ -27,6 +29,7 @@ export class ManageUserRowComponent implements OnInit {
     },0);
     if(this.row){
       this.entityPath = "entity_configs.user_management_dashboard.actions" ;
+      this.options = this.entityConfigService.getEntityConfig(this.entityPath)['values'];
     }
   }
 
@@ -48,7 +51,7 @@ export class ManageUserRowComponent implements OnInit {
 
   getTooltipMsg():string{
     if(this.isReopenInprocess()){
-      return "Case reopening is in process. Please wait to delete user.";
+      return "Case reopening is in process. Please wait to take any action.";
     }else if(this.isWhitelistPending()){
       return "Cannot delete a user or update ethereum address when whitelisting has started. Please try again once the process is complete.";
     }
@@ -80,6 +83,24 @@ export class ManageUserRowComponent implements OnInit {
 
   onUpdateEthAddr(  ) {
     this.updatedEthAddressEvent.emit(this.row);
+  }
+
+  isVisible( option ){
+    let is_kyc_submitted = this.row && this.row.is_kyc_submitted;
+    if( option.value == 'update_eth_addr') {
+      return this.appConfigService.hasEthereumAddressField() && is_kyc_submitted ;
+    }
+    return true;
+  }
+
+  isDisabled( option ){
+    return this.isReopenInprocess() || this.isWhitelistPending() ;
+  }
+
+  ngAfterContentInit(){
+    setTimeout(function(){
+      $('.selectpicker').selectpicker();
+    } , 100);
   }
 
 }
