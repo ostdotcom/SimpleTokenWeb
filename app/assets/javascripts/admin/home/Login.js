495191eb25e96daf39b6aed56343ef9aef45ef7a
aman-barbaria <amanbarbaria@gmail.com>
2019-02-06 17:38:25 +0530

    redirect url on login success

--- app/assets/javascripts/admin/home/Login.js
+++ app/assets/javascripts/admin/home/Login.js
@@ -1,68 +1,71 @@
 ;
 (function (window) {
 
-  var homeNs = ns("simpletokenadmin.home"),
-    adminUtilsNs = ns("simpletokenadmin.utils"),
-    utilities = ns("simpletoken.utilities"),
+    var homeNs = ns("simpletokenadmin.home"),
+        adminUtilsNs = ns("simpletokenadmin.utils"),
+        utilities = ns("simpletoken.utilities"),
 
-      oThis;
+        oThis;
 
-  homeNs.login = oThis = {
+    homeNs.login = oThis = {
 
-    jLoginForm: null,
+        jLoginForm: null,
 
-    init: function (config) {
+        init: function (config) {
 
-      oThis.jLoginForm = $('#adminLoginForm');
-      //test dummy change
-      console.log("test");
-      oThis.jLogin = $('#adminLogin');
-        oThis.formHelper = oThis.jLoginForm.formHelper({
-            success : function ( response ) {
+            oThis.jLoginForm = $('#adminLoginForm');
+            //test dummy change
+            console.log("test");
+            oThis.jLogin = $('#adminLogin');
+            oThis.formHelper = oThis.jLoginForm.formHelper({
+                success: function (response) {
                     if (response.success == true) {
-                        window.location = '/admin/authentication';
+
+                        var data = response.data,
+                            redirect_url = data['redirect_url'];
+                        window.location = redirect_url;
+                        // window.location = '/admin/authentication';
                         return false;
                     } else {
-                        if (response.err && response.err.code === "is_deleted" ){
+                        if (response.err && response.err.code === "is_deleted") {
                             oThis.showDeactivated();
                         }
+                    }
+                },
+                complete: function (response) {
+                    if (typeof grecaptcha != 'undefined') {
+                        grecaptcha.reset();
+                    }
+
                 }
-            },
-            complete: function ( response ) {
-                if(typeof grecaptcha  != 'undefined'){
-                    grecaptcha.reset();
+            });
+            oThis.bindButtonActions();
+
+        },
+
+        bindButtonActions: function () {
+            oThis.jLoginForm.on("beforeSubmit", function (event) {
+                if (!oThis.isCaptchaValid) {
+                    event.preventDefault();
                 }
+            });
 
-            }
-        });
-        oThis.bindButtonActions();
-
-    },
-
-    bindButtonActions: function () {
-        oThis.jLoginForm.on("beforeSubmit", function (event) {
-            if ( !oThis.isCaptchaValid ) {
-                event.preventDefault();
-            }
-        });
-
-        oThis.jLogin.off('click').on('click' , function () {
-            oThis.isCaptchaValid = utilities.validateCaptcha( oThis.jLoginForm );
-           // oThis.formHelper.jForm.submit();
-        });
-    },
+            oThis.jLogin.off('click').on('click', function () {
+                oThis.isCaptchaValid = utilities.validateCaptcha(oThis.jLoginForm);
+                // oThis.formHelper.jForm.submit();
+            });
+        },
 
 
+        showDeactivated: function () {
+            $('#loginForm').hide();
+            $('#accountDeactivated').show();
+        }
 
-    showDeactivated: function () {
-      $('#loginForm').hide();
-      $('#accountDeactivated').show();
-    }
+    };
 
-  };
-
-  $(document).ready(function () {
-    oThis.init({i18n: {}});
-  });
+    $(document).ready(function () {
+        oThis.init({i18n: {}});
+    });
 
 })(window);
