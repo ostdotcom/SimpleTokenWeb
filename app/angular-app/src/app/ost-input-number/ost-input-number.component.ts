ab8c1c778fb3dcf9be82eed9ab1efd868a6e5ddf
Preshita Shirke <preshita@ost.com>
2019-02-13 18:12:21 +0530

    minor fix

--- app/angular-app/src/app/ost-input-number/ost-input-number.component.ts
+++ app/angular-app/src/app/ost-input-number/ost-input-number.component.ts
@@ -26,6 +26,7 @@ export class OstInputNumberComponent implements ControlValueAccessor {
   set modelValue(val) {
     this._modelValue = val;
     this.propagateChange(this._modelValue);
+    this.onTouchedCallback();
   }
 
   constructor() { }
@@ -50,14 +51,12 @@ export class OstInputNumberComponent implements ControlValueAccessor {
   increment(){
     if( this.modelValue >= this.minValue && this.modelValue < this.maxValue) {
       this.modelValue++;
-      this.onTouchedCallback();
     };
   }
 
   decrement(){
     if( this.modelValue <= this.maxValue && this.modelValue > this.minValue) {
       this.modelValue--;
-      this.onTouchedCallback();
     };
   }
 
