85d800b1e2e61866439627ec5cbcadd9ffeb808c
Shraddha <shraddha.patil@ost.com>
2019-05-23 19:10:45 +0530

    minor fix

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,7 @@
 module HomeHelper
 
   # check if cookie consent banner is to be shown
-  def show_cookie_consent_banner?
+  def show_cookie_consent_banner_admin?
     ['forgot_password', 'login', 'activate_account'].include?(params[:action])
   end
 
